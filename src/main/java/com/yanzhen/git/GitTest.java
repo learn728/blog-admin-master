de98255153f564f7e16ee1041079673a83ec4580
fzp <fzp@728>
2022-07-31 19:49:05 +0800

    version fzp version 2

--- src/main/java/com/yanzhen/git/GitTest.java
+++ src/main/java/com/yanzhen/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest{
     public static void main(String[] args) {
         System.out.println("gitnb");
         System.out.println("支线分支");
+        System.out.println("fzp 添加一次");
     }
 }
